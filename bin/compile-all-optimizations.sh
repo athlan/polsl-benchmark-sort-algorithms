7e19772c9dc36f2de75e9ea791137d90542d1b65
Athlan <athlan@vgroup.pl>
2014-03-08 10:47:57 +0100

    Compile tool in all gcc optimization modes.

---
+++ bin/compile-all-optimizations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd code
+gcc -O0 benchmark.c sort_algorithms/*.c -o ../target/benchmark-o0
+gcc -O1 benchmark.c sort_algorithms/*.c -o ../target/benchmark-o1
+gcc -O2 benchmark.c sort_algorithms/*.c -o ../target/benchmark-o2
+gcc -O3 benchmark.c sort_algorithms/*.c -o ../target/benchmark-o3
+cd ..
+
+chmod +x ./target/benchmark*
