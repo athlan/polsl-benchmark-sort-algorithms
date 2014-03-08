1208b92d833b31497a0742944b0bec3991c80a5a
Athlan <athlan@vgroup.pl>
2014-03-08 09:36:55 +0100

    Rand dataset script added.

---
+++ bin/rand-dataset.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+# Script generated random values between MIN_VALUE and MAX_VALUE
+# exactly COUNT times in each line. If COUNT is not defined,
+# one number will be generated
+# 
+# Author: Piotr Pelczar
+
+function usage {
+	echo "usage: rand-dataset.sh MIN_VALUE MAX_VALUE [COUNT]";
+}
+
+min="$1"
+max="$2"
+count="$3"
+
+if [ -z $min ]; then echo "Missing MIN_VALUE" && usage; exit; fi;
+if [ -z $max ]; then echo "Missing MAX_VALUE" && usage; exit; fi;
+if [ -z $count ]; then count=1; fi;
+
+diff=$((max - min + 1));
+
+i=0;
+while [ $i -lt $count ] ; do
+    echo $(( ( RANDOM % diff ) + min ));
+    i=$[i + 1]
+done
