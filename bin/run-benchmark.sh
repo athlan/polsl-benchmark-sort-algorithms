4b94716e6ad0cad1ebe150d7535f5331ebde759d
Athlan <athlan@vgroup.pl>
2014-03-09 16:42:39 +0100

    Results added.

--- bin/run-benchmark.sh
+++ bin/run-benchmark.sh
@@ -4,8 +4,8 @@
 arr_count=(100 1000 10000 100000)
 
 # array of possible key lengths
-#arr_keylength=(100 1000 10000 65000)
-arr_keylength=(100 1000)
+arr_keylength=(100 1000 10000 65000)
+#arr_keylength=(100 1000)
 
 # number of sort iterations
 iterations=100
@@ -19,7 +19,7 @@ for n in "${arr_count[@]}" ; do
     ((n > arr_count_max)) && arr_count_max=$n
 done
 
-echo -e "optimization level;algorithm iterations;tab size;bucket sort time;counting sort time;";
+echo -e "optimization level;key range max;algorithm iterations;tab size;bucket sort time;counting sort time;";
 
 for r_keylength in ${arr_keylength[*]}
 do	
@@ -31,25 +31,30 @@ do
 	for r_count in ${arr_count[*]}
 	do
 		if [ $all_compilations -gt 0 ]; then
-			printf "none;";
+			printf "0;";
+			printf "$r_keylength;";
 			cat dataset.tmp | ../target/benchmark-o0 $iterations $r_count bucket counting
 			echo "";
 			
 			printf "1;";
+			printf "$r_keylength;";
 			cat dataset.tmp | ../target/benchmark-o1 $iterations $r_count bucket counting
 			echo "";
 			
 			printf "2;";
+			printf "$r_keylength;";
 			cat dataset.tmp | ../target/benchmark-o2 $iterations $r_count bucket counting
 			echo "";
 			
 			printf "3;";
+			printf "$r_keylength;";
 			cat dataset.tmp | ../target/benchmark-o3 $iterations $r_count bucket counting
 			echo "";
 			
 		else
 			
-			printf "none;";
+			printf "0;";
+			printf "$r_keylength;";
 			cat dataset.tmp | ../target/benchmark $iterations $r_count bucket counting
 			echo "";
 		fi
